fa0f5ace7555aa91c3582dc3211bd1d7e5607825
cnina <cnina@wustl.edu>
2022-04-24 21:12:48 -0500

    move to gh-pages

---
+++ src/AddNew 2.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// function AddNew({setInputTxt, todoItems, setTodoItems, inputTxt}){
+//     const inputTextDo = (t) => {
+//         console.log(t.target.value);
+//         setInputTxt(t.target.value);
+//     };
+
+//     const submitTextDo = (t) => {
+//         t.preventDefault();
+//         setTodoItems([
+//             ...todoItems, {text: inputTxt, completed: false, id: Math.random()*100, user:"124b10-d2cdcf-95e1c5-67d7bf-35b4b9"}
+//         ]);
+//         setInputTxt("");
+//     };
+
+class AddNew extends Component {
+    constructor(props) {
+        super(props);
+        this.addTodo = this.addTodo.bind(this);
+    };
+
+    addTodo(event) {
+        event.preventDefault();
+        const thisObject = this;
+        const eventText = document.getElementById("newToDo").value;
+        
+        thisObject.props.makeTodo(eventText);
+
+        document.getElementById("newToDo").value = "";
+    }
+
+    render(){
+        return (
+            <span>
+            <form id="addNew" onSubmit={this.addTodo}>
+                <input  
+                    type="text" id="newToDo" 
+                    placeholder="add a new item">
+                </input>
+                <button id="button"> + </button>
+                <button type="button" onClick={this.props.sortTodos} onKeyDown={this.handleKeyDown} id="sort"> sort </button>
+            </form>
+            </span>
+        );
+    }
+    }
+
+export default AddNew;
