6cc0b642bd911667aab0603e3aba0e22d5f2edfa
cnina <cnina@wustl.edu>
2022-04-24 22:21:39 -0500

    Updates

---
+++ precache-manifest.fa6b5e9706e7372ea1f8bfc90e22bb4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d81ed022407c33d28d9",
+    "url": "/09-todo-react-nina-cw/static/css/main.5e530412.chunk.css"
+  },
+  {
+    "revision": "9d81ed022407c33d28d9",
+    "url": "/09-todo-react-nina-cw/static/js/main.9d81ed02.chunk.js"
+  },
+  {
+    "revision": "591d202b2cb6a38a53d4",
+    "url": "/09-todo-react-nina-cw/static/js/1.591d202b.chunk.js"
+  },
+  {
+    "revision": "3679fa083ce4ec61e770",
+    "url": "/09-todo-react-nina-cw/static/js/runtime~main.3679fa08.js"
+  },
+  {
+    "revision": "5539c8d47a742334977a42ccd7433844",
+    "url": "/09-todo-react-nina-cw/index.html"
+  }
+];
